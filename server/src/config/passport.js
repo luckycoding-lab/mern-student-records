5074557ffbd1f1220616f723bf093aee151ed9c3
Lucky Kumar Shocking <lokeshrathor5929@gmail.com>
2026-09-09 12:28:59 +0530

    fix: Google Callback URL

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -12,8 +12,8 @@ export const configurePassport = () => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          process.env.GOOGLE_CLIENT_URL ||
-          "http://localhost:5000/api/auth/google/callback",
+          process.env.GOOGLE_CALLBACK_URL ||
+          "https://mern-student-records.onrender.com/api/auth/google/callback",
         proxy: true,
       },
       async (accessToken, refreshToken, profile, done) => {
